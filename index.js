b8327cc0b054a34038a481c47005bb483e4dc635
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-25 23:34:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const badges = ['Lisa', 'Kaitlin', 'Jan'];
+const badges = ['Lisa', 'Kaitlin', 'Jan', 'Joe', 'Gabe'];
 
 function printBadges (badges) {
   for (let i = 0; i < badges.length; i++) {
