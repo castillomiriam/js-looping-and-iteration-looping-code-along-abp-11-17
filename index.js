c9a8f682702e3cb5f21b34f1de3a34bc980aa759
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-25 23:02:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+
+function findMatching(drivers, name){
+  let matchingDrivers= drivers.filter(function(driver){
+    if (name.toLowerCas() === driver.toLowerCase()){
+      return true
+    } else {
+      return false
+    }
+  })
+
+      
