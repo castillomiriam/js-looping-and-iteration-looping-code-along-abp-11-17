ff862a68be9cc93f721d369f0893f870c6330b36
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-25 23:24:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function printBadges() {
-  
+
+const badges = ['Lisa', 'Kaitlin', 'Jan'];
+ 
+function printBadges (badges) {
+  for (let i = 0; i < badges.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return badges;
 }
+ 
+printBadges(badges);
