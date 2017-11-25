94a00b1cad36baeebefc3a4b81e0684fc4915cd0
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-25 23:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function printBadges (badges) {
     console.log(`Welcome ${badges[i]}! You are employee #${[i]}`);
   }
 
- function printBadges(['Joe', 'Gabe']); {
+ function printBadges('Joe', 'Gabe'); {
    for (let i=0<badges.length; i++) {
      console.log(`Welcome ${badges[i]}! You are employee #${[i]}`);
    }
