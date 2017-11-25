42e6b8c8ecec46ebf1e50292d754f0bbec063c5d
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-25 23:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-function findMatching(drivers, name){
-  let matchingDrivers= drivers.filter(function(driver){
-    if (name.toLowerCas() === driver.toLowerCase()){
-      return true
-    } else {
-      return false
-    }
-  })
+
 
       
